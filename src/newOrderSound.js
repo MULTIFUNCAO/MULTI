81b7ce962078b4323e941039c7832b996c4d018e
MULTIFUNCAO <thyagosantos1401@gmail.com>
2026-05-07 16:14:54 -0300

    feat: som mais alto e longo 4x

--- src/newOrderSound.js
+++ src/newOrderSound.js
@@ -2,27 +2,27 @@ let _playing=false;
 export const playNewOrderSound = () => {
   if(_playing) return;
   _playing=true;
-  setTimeout(()=>_playing=false,5000);
+  setTimeout(()=>_playing=false,8000);
   let count=0;
   const bip = () => {
     const ctx = new (window.AudioContext || window.webkitAudioContext)();
-    const playBeep = (freq, start, duration) => {
+    const playBeep = (freq, start, duration, vol) => {
       const osc = ctx.createOscillator();
       const gain = ctx.createGain();
       osc.connect(gain);
       gain.connect(ctx.destination);
       osc.frequency.value = freq;
-      osc.type = 'sine';
-      gain.gain.setValueAtTime(0.4, ctx.currentTime + start);
+      osc.type = 'triangle';
+      gain.gain.setValueAtTime(vol, ctx.currentTime + start);
       gain.gain.exponentialRampToValueAtTime(0.001, ctx.currentTime + start + duration);
       osc.start(ctx.currentTime + start);
-      osc.stop(ctx.currentTime + start + duration);
+      osc.stop(ctx.currentTime + start + duration + 0.05);
     };
-    playBeep(880, 0, 0.12);
-    playBeep(1100, 0.15, 0.12);
-    playBeep(1320, 0.3, 0.2);
+    playBeep(523, 0,    0.35, 0.8);
+    playBeep(659, 0.4,  0.35, 0.8);
+    playBeep(784, 0.8,  0.5,  0.9);
     count++;
-    if(count < 3) setTimeout(bip, 800);
+    if(count < 4) setTimeout(bip, 1800);
   };
   bip();
 };
