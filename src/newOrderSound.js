337d4aecb0135e0dd5b8e5726a70e2bf8ab311fc
MULTIFUNCAO <thyagosantos1401@gmail.com>
2026-05-07 15:42:26 -0300

    feat: som novo pedido

---
+++ src/newOrderSound.js
@@ -0,0 +1,21 @@
+
+// Som de novo pedido - Multi
+export const playNewOrderSound = () => {
+  const ctx = new (window.AudioContext || window.webkitAudioContext)();
+  const playBeep = (freq, start, duration) => {
+    const osc = ctx.createOscillator();
+    const gain = ctx.createGain();
+    osc.connect(gain);
+    gain.connect(ctx.destination);
+    osc.frequency.value = freq;
+    osc.type = 'sine';
+    gain.gain.setValueAtTime(0.3, ctx.currentTime + start);
+    gain.gain.exponentialRampToValueAtTime(0.001, ctx.currentTime + start + duration);
+    osc.start(ctx.currentTime + start);
+    osc.stop(ctx.currentTime + start + duration);
+  };
+  playBeep(880, 0, 0.15);
+  playBeep(1100, 0.2, 0.15);
+  playBeep(880, 0.4, 0.15);
+  playBeep(1320, 0.6, 0.3);
+};
