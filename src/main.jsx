cb99f945244c32167862761e5f293ce89e588f96
MULTIFUNCAO <thyagosantos1401@gmail.com>
2026-05-23 17:26:45 -0300

    debug: error handler no main.jsx para capturar erro silencioso

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+window.onerror = function(msg, src, line, col, err) {
+  document.getElementById('root').innerHTML = '<pre style="color:red;padding:20px;font-size:12px">ERRO: ' + msg + '\nLinha: ' + line + '\n' + (err ? err.stack : '') + '</pre>';
+};
+
+window.onunhandledrejection = function(e) {
+  document.getElementById('root').innerHTML = '<pre style="color:red;padding:20px;font-size:12px">PROMISE ERROR: ' + e.reason + '</pre>';
+};
+
 createRoot(document.getElementById('root')).render(
   <StrictMode><App /></StrictMode>
 )
-
-
-
