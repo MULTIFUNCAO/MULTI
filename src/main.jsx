6488296b958e94bc7b2b5e07560a8bc3aaebab0f
MULTIFUNCAO <thyagosantos1401@gmail.com>
2026-05-24 00:22:52 -0300

    fix: main.jsx sem string literal error

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,10 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.jsx"
 
-class ErrorBoundary extends Error {}
-
-window.onerror = function(msg,src,line,col,err){
-  document.getElementById("root").innerHTML = "<pre style=color:red>ERRO: "+msg+"
-Linha:"+line+"
-"+(err&&err.stack||"")+"</pre>";
-};
-window.onunhandledrejection = function(e){
-  document.getElementById("root").innerHTML = "<pre style=color:red>PROMISE: "+e.reason+"</pre>";
+window.onerror = function(msg, src, line, col, err) {
+  document.getElementById("root").innerHTML = "<pre style='color:red'>ERRO: " + msg + " Linha:" + line + " " + (err && err.stack || "") + "</pre>";
 };
 
-const root=createRoot(document.getElementById("root"));setTimeout(()=>{console.log("ROOT HTML:",document.getElementById("root").innerHTML.slice(0,500));},2000);root.render(<StrictMode><App /></StrictMode>)
+createRoot(document.getElementById("root")).render(
+  <StrictMode><App /></StrictMode>
+)
