db8ad6b243403dd80ff6179d5522ec2210376187
MULTIFUNCAO <thyagosantos1401@gmail.com>
2026-05-24 00:17:29 -0300

    debug: onerror global para capturar erro silencioso

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,16 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.jsx"
+
+class ErrorBoundary extends Error {}
+
+window.onerror = function(msg,src,line,col,err){
+  document.getElementById("root").innerHTML = "<pre style=color:red>ERRO: "+msg+"
+Linha:"+line+"
+"+(err&&err.stack||"")+"</pre>";
+};
+window.onunhandledrejection = function(e){
+  document.getElementById("root").innerHTML = "<pre style=color:red>PROMISE: "+e.reason+"</pre>";
+};
+
 createRoot(document.getElementById("root")).render(<StrictMode><App /></StrictMode>)
