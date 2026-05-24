6603cf238f89d0527712a49f94abc9dcf41bd614
MULTIFUNCAO <thyagosantos1401@gmail.com>
2026-05-24 00:02:06 -0300

    fix: entry point com nome fixo unico

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: "assets/app-mpj6t021-[hash].js"
+        entryFileNames: "assets/multi-mpj6yrqo.js",
+        chunkFileNames: "assets/chunk-[hash].js",
+        assetFileNames: "assets/[name]-[hash][extname]"
       }
     }
   }
