3665104be887af619440ee67455f4e276787c598
MULTIFUNCAO <thyagosantos1401@gmail.com>
2026-05-28 18:03:31 -0300

    fix: vite config padrao sem nome fixo

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,5 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: "assets/multi-mpplhnra.js",
-        chunkFileNames: "assets/chunk-[hash].js",
-        assetFileNames: "assets/[name]-[hash][extname]"
-      }
-    }
-  }
+  plugins: [react()]
 })
