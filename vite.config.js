0a04f3473a26277fe96ff10934ca2bebc8b476bd
MULTIFUNCAO <thyagosantos1401@gmail.com>
2026-05-28 11:31:00 -0300

    fix: novo bundle

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: "assets/multi-mpplagfc.js",
+        entryFileNames: "assets/multi-mpplc40s.js",
         chunkFileNames: "assets/chunk-[hash].js",
         assetFileNames: "assets/[name]-[hash][extname]"
       }
