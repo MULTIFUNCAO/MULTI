de577e7be62c248a441efe9681fdfaec5615bff0
MULTIFUNCAO <thyagosantos1401@gmail.com>
2026-05-28 18:22:51 -0300

    debug: app minimo sem import App

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,6 @@
 import { createRoot } from "react-dom/client"
+import React from "react"
 
-const root = document.getElementById("root")
-root.innerHTML = "<h1 style='color:blue'>JS FUNCIONANDO</h1>"
-
-try {
-  const { default: App } = await import("./App.jsx")
-  root.innerHTML = ""
-  createRoot(root).render(App ? "<div>App carregado</div>" : "<div>App undefined</div>")
-} catch(e) {
-  root.innerHTML = "<pre style='color:red'>ERRO NO IMPORT: " + e.message + "\n" + e.stack + "</pre>"
-}
+createRoot(document.getElementById("root")).render(
+  React.createElement("h1", {style:{color:"blue",padding:20}}, "MULTI APP FUNCIONANDO")
+)
