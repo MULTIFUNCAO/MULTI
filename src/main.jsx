63e58bbe817b7baf6e44caf32afb13e03e77a3c8
MULTIFUNCAO <thyagosantos1401@gmail.com>
2026-05-28 18:31:05 -0300

    fix: main.jsx restaurado

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import { createRoot } from "react-dom/client"
 import React from "react"
+import { createRoot } from "react-dom/client"
 import App from "./App.jsx"
 
 createRoot(document.getElementById("root")).render(
-  React.createElement(App)
+  <React.StrictMode><App /></React.StrictMode>
 )
